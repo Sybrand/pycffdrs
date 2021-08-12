2b2889af6983c4e6c4cb7f160bc099b0b4db826c
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-08-11 21:57:52 -0700

    fixed buiCalc bug and added tests

---
+++ pycffdrs/buiCalc.py
@@ -0,0 +1,52 @@
+"""
+All code is based on the R project: https://cran.r-project.org/package=cffdrs
+
+Comments taken from the R project relating to this module are:
+
+"All code is based on a C code library that was written by Canadian
+Forest Service Employees, which was originally based on
+the Fortran code listed in the reference below. All equations
+in this code refer to that document.
+
+Equations and FORTRAN program for the Canadian Forest Fire 
+Weather Index System. 1985. Van Wagner, C.E.; Pickett, T.L. 
+Canadian Forestry Service, Petawawa National Forestry 
+Institute, Chalk River, Ontario. Forestry Technical Report 33. 
+18 p.
+
+Additional reference on FWI system
+
+Development and structure of the Canadian Forest Fire Weather 
+Index System. 1987. Van Wagner, C.E. Canadian Forestry Service,
+Headquarters, Ottawa. Forestry Technical Report 35. 35 p."
+"""
+from math import pow
+from numba import jit
+
+@jit(nopython=True)
+def buiCalc(dmc: float, dc: float) -> float:
+    """
+    Buildup Index Calculation. A single bui value.
+
+    Keyword arguments:
+    dc -- Drought Code
+    dmc -- Duff Moisture Code
+    """
+    #Eq. 27a
+    if dmc == 0 and dc == 0:
+        bui1 = 0
+    else:
+        bui1 = 0.8 * dc * dmc/(dmc + 0.4 * dc)
+    #Eq. 27b - next 3 lines
+    if dmc == 0:
+        p = 0
+    else:
+        p = (dmc - bui1)/dmc
+    cc = 0.92 + pow((0.0114 * dmc), 1.7)
+    bui0 = dmc - cc * p
+    #Constraints
+    if bui0 < 0:
+        bui0 = 0
+    if bui1 < dmc:
+        bui1 = bui0
+    return bui1
--- tests/test_pycffdrs.py
+++ tests/test_pycffdrs.py
@@ -1,9 +1,37 @@
-from pycffdrs import __version__
 
+"""
+Test pycffdrs by comparing it to the R cffdrs package.
+
+NOTE: This test assumes you have R and the cffdrs package installed!
+"""
+import random
+from rpy2.robjects.packages import importr
+from pycffdrs import __version__
 from pycffdrs.fwiCalc import fwiCalc
+from pycffdrs.buiCalc import buiCalc
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.0.3'
 
 def test_fwiCalc():
-    assert fwiCalc(10, 10) == 10.135272037131825
+    # load cffdrs R package.
+    cffdrs = importr('cffdrs')
+    # using a seed (for determinism) - run through a bunch of random iterations comparing our output
+    # with that of the R package.
+    random.seed(42)
+    for _ in range(100):
+        isi = random.uniform(0, 100)
+        bui = random.uniform(0, 100)
+        assert fwiCalc(isi, bui) == cffdrs._fwiCalc(isi, bui)[0]
+
+
+def test_buiCalc():
+    # load cffdrs R package.
+    cffdrs = importr('cffdrs')
+    # using a seed (for determinism) - run through a bunch of random iterations comparing our output
+    # with that of the R package.
+    random.seed(42)
+    for _ in range(100):
+        dc = random.uniform(0, 600)
+        dmc = random.uniform(0, 100)
+        assert buiCalc(dc, dmc) == cffdrs._buiCalc(dc, dmc)[0]
