331cfc5652de84b5cdb346e131899339024d6aa6
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-05-18 22:12:44 -0700

    translating more r-code

--- pycffdrs/hffmc.py
+++ pycffdrs/hffmc.py
@@ -1,9 +1,12 @@
 """
 All code and comments based on the R project: https://cran.r-project.org/package=cffdrs
 """
+from warnings import showwarning as warning
+from numpy import ndarray
+import numpy as np
 
 
-def hffmc(weatherstream, ffmc_old=85, time_step=1,
+def hffmc(weatherstream: ndarray, ffmc_old=85, time_step=1,
           calc_step=False, batch=True, hourlyFWI=False):
     #############################################################################
     # Description: Diurnal (Hourly) Fine Fuel Moisture Code Calculation. Most of
@@ -47,125 +50,129 @@ def hffmc(weatherstream, ffmc_old=85, time_step=1,
     #
     #############################################################################
     t0 = time_step
-    # TODO: what does this translate to?
-    # NOTE: Assuming weatherstream is a numpy array, we can simply change the field names
-    # as follows:
-    # weatherstream.dtype.names = ('temp', 'rh', 'ws', 'precip', 'hourly', 'bui')
-    # SO: we'll just iterate though the existing name, and lowercase them.
-#   names(weatherstream) <- tolower(names(weatherstream))
-  # set up number of stations
-    # if (batch):
-#     if ("id" %in% names(weatherstream)) {
-#       n <- length(unique(weatherstream$id))
-#       if(length(unique(weatherstream[1:n,"id"])) != n){
-#         stop("Multiple stations have to start and end at the same dates/time,
-#              and the data must be sorted by date/time and id")
-#       }
-#     } else {
-#       n <- 1
-#     }
-#   }else{
-#     n <- nrow(weatherstream)
-#   }
+    weatherstream.dtype.names = [name.lower() for name in weatherstream.dtype.names]
+    # set up number of stations
+    if (batch):
+        if "id" in weatherstream.dtype.names:
+            n = len(np.unique(weatherstream['id']))
+            if len(np.unique(weatherstream["id"][1:n])) != n:
+                raise "Multiple stations have to start and end at the same dates/time, and the data must be sorted by date/time and id"
+        else:
+            n = 1
+    else:
+        n = len(weatherstream)
 
-#   if (length(ffmc_old) == 1 & n > 1){
-#     Fo <- rep(ffmc_old, n)
-#   } else {
-#     Fo <- ffmc_old
-#   }
+    if len(ffmc_old) == 1 and n > 1:
+        Fo = np.repeat(ffmc_old, n)
+    else:
+        Fo = ffmc_old
 
-#   #set some local scope variables
-#   Tp <- weatherstream$temp
-#   H  <- weatherstream$rh
-#   W  <- weatherstream$ws
-#   ro <- weatherstream$prec
+    # set some local scope variables
+    if "temp" in weatherstream.dtype.names:
+        Tp = weatherstream["temp"]
+    else:
+        Tp = None
+    if "rh" in weatherstream.dtype.names:
+        H = weatherstream["rh"]
+    else:
+        H = None
+    if "ws" in weatherstream.dtype.names:
+        W = weatherstream["ws"]
+    else:
+        W = None
+    if "prec" in weatherstream.dtype.names:
+        ro = weatherstream["prec"]
+    else:
+        ro = None
 
-#   #Check that the parameters are correct
-#   if (calc.step){
-#     hr <- weatherstream$hr
-#     if (!exists("hr") | is.null(hr))
-#       warning("hour value is missing!")
-#   }
-#   if (!exists("Tp") | is.null(Tp))
-#     warning("temperature (temp) is missing!")
-#   if (!exists("ro") | is.null(ro))
-#     warning("precipitation (prec) is missing!")
-#   if (!exists("W") | is.null(W))
-#     warning("wind speed (ws) is missing!")
-#   if (!exists("H") | is.null(H))
-#     warning("relative humidity (rh) is missing!")
-#   if (length(H)%%n != 0)
-#     warning("Weatherstream do not match with number of weather stations")
-#   #Length of weather run
-#   n0 <- length(H) / n
-#   f <- NULL
-#   #For each day in the run
-#   for (i in 1:n0){
-#     #k is the data for all stations by day
-#     k <- ((i - 1) * n + 1):(i * n)
-#     if (calc.step & i > 1) {
-#       t0 <- ifelse(n0 > 1, hr[k] - hr[k-n], t0)
-#       t0 <- ifelse(t0 == -23, 1, t0)
-#       t0 <- ifelse(t0 < 0, -1 * t0, t0)
-#     }
-#     #Eq. 1 (with a more precise multiplier than the daily)
-#     mo <- 147.27723 * (101 - Fo)/(59.5 + Fo)
-#     rf <- ro[k]
-#     #Eqs. 3a & 3b (Van Wagner & Pickett 1985)
-#     mr <- ifelse(mo <= 150,
-#             mo + 42.5 * rf * exp(-100 / (251 - mo)) * (1 - exp(-6.93 / rf)),
-#             mo + 42.5 * rf * exp(-100 / (251 - mo)) * (1 - exp(-6.93 / rf)) +
-#               0.0015 * ((mo - 150)^2) * (rf^0.5))
-#     #The real moisture content of pine litter ranges up to about 250 percent,
-#     # so we cap it at 250
-#     mr <- ifelse(mr > 250, 250, mr)
-#     mo <- ifelse(ro[k] > 0.0, mr, mo)
-#     #Eq. 2a Equilibrium moisture content from drying
-#     Ed <- 0.942 * (H[k]^0.679) + 11 * exp((H[k] - 100) / 10) + 0.18 *
-#           (21.1 - Tp[k]) * (1 - exp(-0.115 * H[k]))
-#     #Eq. 3a Log drying rate at the normal temperature of 21.1C
-#     ko <- 0.424 * (1 - (H[k] / 100)^1.7) + 0.0694 * (W[k]^0.5) *
-#           (1 - (H[k] / 100)^8)
-#     #Eq. 3b
-#     kd <- ko * 0.0579 * exp(0.0365 * Tp[k])
-#     #Eq. 8 (Van Wagner & Pickett 1985)
-#     md <- Ed + (mo - Ed) * (10^(-kd * t0))
-#     #Eq. 2b Equilibrium moisture content from wetting
-#     Ew <- 0.618 * (H[k]^0.753) + 10 * exp((H[k] - 100) / 10) + 0.18 *
-#           (21.1 - Tp[k]) * (1 - exp(-0.115 * H[k]))
-#     #Eq. 7a Log wetting rate at the normal temperature of 21.1 C
-#     k1 <- 0.424 * (1 - ((100 - H[k]) / 100)^1.7) + 0.0694 *
-#       (W[k]^0.5) * (1 - ((100 - H[k]) / 100)^8)
-#     #Eq. 4b
-#     kw <- k1 * 0.0579 * exp(0.0365 * Tp[k])
-#     #Eq. 8 (Van Wagner & Pickett 1985)
-#     mw <- Ew - (Ew - mo) * (10^(-kw * t0))
-#     #Constraints
-#     m <- ifelse(mo > Ed, md, mw)
-#     m <- ifelse(Ed >= mo & mo >= Ew, mo, m)
-#     #Eq. 6 - Final hffmc calculation (modified 3rd constant to 147.27723)
-#     Fo <- 59.5 * (250 - m) / (147.27723 + m)
-#     Fo <- ifelse(Fo <=0, 0, Fo)
-#     f <- c(f, Fo)
-#   }
-#   #Calculate hourly isi and fwi
-#   if (hourlyFWI){
-#     bui <- weatherstream$bui
-#     if (!exists("bui") | is.null(bui)){
-#       warning("Daily BUI is required to calculate hourly FWI")
-#     } else {
-#       #Calculate ISI
-#       isi <- .ISIcalc(f, W, FALSE)
-#       #Calculate FWI
-#       fwi <- .fwiCalc(isi, bui)
-#       #Calculate DSR
-#       dsr <- 0.0272 * (fwi^1.77)
-#       #Put all data into a data.frame to return
-#       output <- cbind(weatherstream,
-#                       data.frame(ffmc = f, isi = isi, fwi = fwi, dsr = dsr))
-#       return(output)
-#     }
-#     #otherwise just return hffmc
-#   } else {
-#     return(f)
-#   }
+    # Check that the parameters are correct
+    if calc_step:
+        if "hr" in weatherstream.dtype.names:
+            hr = weatherstream["hr"]
+        else:
+            hr = None
+    if hr is None:
+        warning("hour value is missing!")
+    if Tp is None:
+        warning("temperature (temp) is missing!")
+    if ro is None:
+        warning("precipitation (prec) is missing!")
+    if W is None:
+        warning("wind speed (ws) is missing!")
+    if H is None:
+        warning("relative humidity (rh) is missing!")
+    if len(H) % n != 0:
+        warning("Weatherstream do not match with number of weather stations")
+    # Length of weather run
+    n0 = len(H) / n
+    f = None
+    # For each day in the run
+    #   for (i in 1:n0){
+    #     #k is the data for all stations by day
+    #     k <- ((i - 1) * n + 1):(i * n)
+    #     if (calc.step & i > 1) {
+    #       t0 <- ifelse(n0 > 1, hr[k] - hr[k-n], t0)
+    #       t0 <- ifelse(t0 == -23, 1, t0)
+    #       t0 <- ifelse(t0 < 0, -1 * t0, t0)
+    #     }
+    #     #Eq. 1 (with a more precise multiplier than the daily)
+    #     mo <- 147.27723 * (101 - Fo)/(59.5 + Fo)
+    #     rf <- ro[k]
+    #     #Eqs. 3a & 3b (Van Wagner & Pickett 1985)
+    #     mr <- ifelse(mo <= 150,
+    #             mo + 42.5 * rf * exp(-100 / (251 - mo)) * (1 - exp(-6.93 / rf)),
+    #             mo + 42.5 * rf * exp(-100 / (251 - mo)) * (1 - exp(-6.93 / rf)) +
+    #               0.0015 * ((mo - 150)^2) * (rf^0.5))
+    #     #The real moisture content of pine litter ranges up to about 250 percent,
+    #     # so we cap it at 250
+    #     mr <- ifelse(mr > 250, 250, mr)
+    #     mo <- ifelse(ro[k] > 0.0, mr, mo)
+    #     #Eq. 2a Equilibrium moisture content from drying
+    #     Ed <- 0.942 * (H[k]^0.679) + 11 * exp((H[k] - 100) / 10) + 0.18 *
+    #           (21.1 - Tp[k]) * (1 - exp(-0.115 * H[k]))
+    #     #Eq. 3a Log drying rate at the normal temperature of 21.1C
+    #     ko <- 0.424 * (1 - (H[k] / 100)^1.7) + 0.0694 * (W[k]^0.5) *
+    #           (1 - (H[k] / 100)^8)
+    #     #Eq. 3b
+    #     kd <- ko * 0.0579 * exp(0.0365 * Tp[k])
+    #     #Eq. 8 (Van Wagner & Pickett 1985)
+    #     md <- Ed + (mo - Ed) * (10^(-kd * t0))
+    #     #Eq. 2b Equilibrium moisture content from wetting
+    #     Ew <- 0.618 * (H[k]^0.753) + 10 * exp((H[k] - 100) / 10) + 0.18 *
+    #           (21.1 - Tp[k]) * (1 - exp(-0.115 * H[k]))
+    #     #Eq. 7a Log wetting rate at the normal temperature of 21.1 C
+    #     k1 <- 0.424 * (1 - ((100 - H[k]) / 100)^1.7) + 0.0694 *
+    #       (W[k]^0.5) * (1 - ((100 - H[k]) / 100)^8)
+    #     #Eq. 4b
+    #     kw <- k1 * 0.0579 * exp(0.0365 * Tp[k])
+    #     #Eq. 8 (Van Wagner & Pickett 1985)
+    #     mw <- Ew - (Ew - mo) * (10^(-kw * t0))
+    #     #Constraints
+    #     m <- ifelse(mo > Ed, md, mw)
+    #     m <- ifelse(Ed >= mo & mo >= Ew, mo, m)
+    #     #Eq. 6 - Final hffmc calculation (modified 3rd constant to 147.27723)
+    #     Fo <- 59.5 * (250 - m) / (147.27723 + m)
+    #     Fo <- ifelse(Fo <=0, 0, Fo)
+    #     f <- c(f, Fo)
+    #   }
+    #   #Calculate hourly isi and fwi
+    #   if (hourlyFWI){
+    #     bui <- weatherstream$bui
+    #     if (!exists("bui") | is.null(bui)){
+    #       warning("Daily BUI is required to calculate hourly FWI")
+    #     } else {
+    #       #Calculate ISI
+    #       isi <- .ISIcalc(f, W, FALSE)
+    #       #Calculate FWI
+    #       fwi <- .fwiCalc(isi, bui)
+    #       #Calculate DSR
+    #       dsr <- 0.0272 * (fwi^1.77)
+    #       #Put all data into a data.frame to return
+    #       output <- cbind(weatherstream,
+    #                       data.frame(ffmc = f, isi = isi, fwi = fwi, dsr = dsr))
+    #       return(output)
+    #     }
+    #     #otherwise just return hffmc
+    #   } else {
+    #     return(f)
+    #   }
